4401302e58a4fcf8e09a0088aa5e4902a938c550
시니 <110521092+Christopher-Emunah-Kim@users.noreply.github.com>
2024-12-04 19:36:13 +0900

    [level 0] Title: 덧셈식 출력하기, Time: 3.12 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181947. 덧셈식 출력하기/덧셈식 출력하기.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int a;
+    int b;
+    scanf("%d %d", &a, &b);
+    
+    cout<< a << " + " << b << " = " << a+b <<endl;
+    
+    return 0;
+}
